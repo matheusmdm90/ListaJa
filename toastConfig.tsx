0aa5f7222066e31c192fab3304cd766e09814a44
matheusmdm90 <mdm.matheusswat@gmail.com>
2026-06-30 07:29:02 -0300

    implemtando o tosat para melhoria nos avisos

---
+++ toastConfig.tsx
@@ -0,0 +1,66 @@
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { Text, View } from "react-native";
+import { ToastConfigParams } from "react-native-toast-message";
+
+/*
+  1. Create the config
+*/
+export const toastConfig = {
+  sucesso2: ({ text1, text2 }: ToastConfigParams<any>) => (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 20,
+        height: 60,
+        width: "90%",
+        backgroundColor: "#111420",
+        borderWidth: 2,
+        borderColor: "green",
+        borderRadius: 20,
+        paddingLeft: 20,
+        paddingVertical: 10,
+      }}
+    >
+      <View>
+        <MaterialCommunityIcons
+          name="check-circle"
+          size={24}
+          color={"#75FB4C"}
+        />
+      </View>
+      <View>
+        <Text style={{ color: "#FFFF", fontWeight: "bold" }}>{text1}</Text>
+        <Text style={{ color: "#FFFF" }}>{text2}</Text>
+      </View>
+    </View>
+  ),
+
+  Erros: ({ text1, text2 }: ToastConfigParams<any>) => (
+    <View
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "center",
+        gap: 20,
+        height: 60,
+        width: "90%",
+        backgroundColor: "#111420",
+        borderWidth: 2,
+        borderColor: "red",
+        borderRadius: 20,
+        paddingLeft: 20,
+        paddingVertical: 10,
+      }}
+    >
+      <View>
+        <MaterialCommunityIcons name="block-helper" size={24} color={"red"} />
+      </View>
+      <View>
+        <Text style={{ color: "#FFFF", fontWeight: "bold" }}>{text1}</Text>
+        <Text style={{ color: "#FFFF" }}>{text2}</Text>
+      </View>
+    </View>
+  ),
+};
