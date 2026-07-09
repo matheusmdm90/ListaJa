704638b443d6fd47ee18199b11d7674fe9b48d82
matheusmdm90 <mdm.matheusswat@gmail.com>
2026-07-09 05:48:10 -0300

    refactor: criando e utilizando hooks para distribuir fuções da aplicação

--- app/index.tsx
+++ app/index.tsx
@@ -1,83 +1,25 @@
 import { useRouter } from "expo-router";
-import { useState } from "react";
 import {
   Keyboard,
   KeyboardAvoidingView,
-  Platform,
   StyleSheet,
   Text,
   TouchableWithoutFeedback,
   View,
 } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import Toast from "react-native-toast-message";
 import Botao from "../Components/Botao";
 import Inputs from "../Components/inputs";
 import Logo from "../Components/Logo";
-import { useApp } from "../Contexts/UserApp";
-import erros from "../utils/errors";
-import { fazerLogin, obterUsuario } from "../utils/requisicao";
+import useLogin from "../hooks/useLogin";
 
 export default function Index() {
   const router = useRouter();
-  const [email, setEmail] = useState("");
-  const [senha, setSenha] = useState("");
-  const [loading, setLoading] = useState(false);
-  const { dadosLogin } = useApp();
-
-  const login = async () => {
-    if (!email?.trim()) {
-      Toast.show({
-        type: "Erros",
-        text1: "Erro ao entrar!",
-        text2: "Email Obrigatorio",
-        position: "top", // 'top' ou 'bottom'
-        visibilityTime: 3000, // 3 segundos
-      });
-      return;
-    }
-    setLoading(true);
-    try {
-      const { data, error } = await fazerLogin({
-        email,
-        password: senha,
-      });
-
-      if (error) {
-        throw error;
-      }
-
-      const id = data.user?.id;
-
-      const { data: dadosUser, error: errorDadosUser } = await obterUsuario({
-        id_usuario: id,
-      });
-      if (errorDadosUser) {
-        throw errorDadosUser;
-      }
-
-      Toast.show({
-        type: "sucesso2",
-        text1: "Sucesso!",
-        text2: "Seu login foi realizado com sucesso.",
-        position: "top", // 'top' ou 'bottom'
-        visibilityTime: 3000, // 3 segundos
-      });
-      dadosLogin(dadosUser);
-      router.replace("/(tabs)/Home");
-    } catch (erro: any) {
-      erros(erro);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const { email, setEmail, loading, senha, setSenha, login } = useLogin();
 
   return (
     <SafeAreaView style={{ flex: 1, backgroundColor: "#101422" }}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === "ios" ? "padding" : "padding"}
-        style={{ flex: 1 }}
-      >
+      <KeyboardAvoidingView behavior="padding" style={{ flex: 1 }}>
         <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
           <View style={styles.container}>
             <Logo />
@@ -149,7 +91,7 @@ const styles = StyleSheet.create({
     color: "#2559F4",
     left: 220,
     fontSize: 12,
-    fontWeight: 400,
+    fontWeight: "400",
   },
 
   cadastro: {
---
+++ hooks/useLogin.ts
@@ -0,0 +1,80 @@
+import { useRouter } from "expo-router";
+import { useState } from "react";
+import Toast from "react-native-toast-message";
+import { useApp } from "../Contexts/UserApp";
+import erros from "../utils/errors";
+import { fazerLogin, obterUsuario } from "../utils/requisicao";
+
+interface UseLoginReturn {
+  email: string;
+  setEmail: (value: string) => void;
+  senha: string;
+  setSenha: (value: string) => void;
+  loading: boolean;
+  login: () => Promise<void>;
+}
+
+const useLogin = (): UseLoginReturn => {
+  const router = useRouter();
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { dadosLogin } = useApp();
+
+  const login = async () => {
+    if (!email?.trim()) {
+      Toast.show({
+        type: "Erros",
+        text1: "Erro ao entrar!",
+        text2: "Email Obrigatorio",
+        position: "top", // 'top' ou 'bottom'
+        visibilityTime: 3000, // 3 segundos
+      });
+      return;
+    }
+    setLoading(true);
+    try {
+      const { data, error } = await fazerLogin({
+        email,
+        password: senha,
+      });
+
+      if (error) {
+        throw error;
+      }
+
+      const id = data.user?.id;
+
+      const { data: dadosUser, error: errorDadosUser } = await obterUsuario({
+        id_usuario: id,
+      });
+      if (errorDadosUser) {
+        throw errorDadosUser;
+      }
+
+      Toast.show({
+        type: "sucesso2",
+        text1: "Sucesso!",
+        text2: "Seu login foi realizado com sucesso.",
+        position: "top", // 'top' ou 'bottom'
+        visibilityTime: 3000, // 3 segundos
+      });
+      dadosLogin(dadosUser);
+      router.replace("/(tabs)/Home");
+    } catch (erro: any) {
+      erros(erro);
+    } finally {
+      setLoading(false);
+    }
+  };
+  return {
+    email,
+    setEmail,
+    senha,
+    setSenha,
+    loading,
+    login,
+  };
+};
+
+export default useLogin;
